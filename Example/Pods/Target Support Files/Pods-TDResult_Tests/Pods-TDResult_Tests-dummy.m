ca05463441150162069fa7e40c0b336f2e7b0519
abhimanu jindal <abhimanyujindal10@gmail.com>
2017-11-21 11:14:08 +0530

    Added example project

---
+++ Example/Pods/Target Support Files/Pods-TDResult_Tests/Pods-TDResult_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TDResult_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TDResult_Tests
+@end
---
+++ Example/TDResult/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TDResult
+//
+//  Created by trident10 on 11/20/2017.
+//  Copyright (c) 2017 trident10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
